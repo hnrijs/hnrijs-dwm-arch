c66f3850ae23ec2202eb4b9416029ca3e4b3af89
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-20 00:28:22 +0300

    Add files via upload

---
+++ config/scripts/rofi-web.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PLATFORMS="YouTube\nReddit\nGitHub\nGemini\nMaps\nTranslate\nWindy"
+
+INPUT=$(echo -e "$PLATFORMS" | rofi -dmenu -p "Web Search" -i)
+
+[ -z "$INPUT" ] && exit 1
+
+TARGET=$(echo "$INPUT" | awk '{print $1}')
+QUERY=$(echo "$INPUT" | cut -d' ' -f2-)
+
+ENCODED_QUERY=$(echo -n "$QUERY" | tr ' ' '+')
+
+if [ "$TARGET" = "$INPUT" ]; then
+    case "$TARGET" in
+        "YouTube")   librewolf "https://youtube.com" & ;;
+        "Reddit")    librewolf "https://reddit.com" & ;;
+        "GitHub")    librewolf "https://github.com" & ;;
+        "Gemini")    librewolf "https://gemini.google.com/" & ;;
+        "Maps")      librewolf "https://google.com/maps" & ;;
+        "Translate") librewolf "https://translate.google.com" & ;;
+        "Windy") librewolf "https://windy.com" & ;;
+    esac
+else
+    case "$TARGET" in
+        "YouTube")   librewolf "https://youtube.com" & ;;
+        "Reddit")    librewolf "https://reddit.com" & ;;
+        "GitHub")    librewolf "https://github.com" & ;;
+        "Gemini")    librewolf "https://gemini.google.com/" & ;;
+        "Maps")      librewolf "https://google.com/maps" & ;;
+        "Translate") librewolf "https://translate.google.com" & ;;
+        "Windy") librewolf "https://windy.com" & ;;
+    esac
+fi

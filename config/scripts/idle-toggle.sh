a5c9ff26d90ffeff68464db9182f122a353a2539
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-20 02:16:58 +0300

    Update idle-toggle.sh

--- config/scripts/idle-toggle.sh
+++ config/scripts/idle-toggle.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-if xset q | grep -q "DPMS is Enabled"; then
+
+# Check if xidlehook is running
+if pgrep -x "xidlehook" > /dev/null; then
+    # Disable Idle Mode
+    killall xidlehook
     xset -dpms
-    xautolock -disable
     notify-send -u normal "Caffeine Mode" "Enabled: Screen will not turn off."
 else
+    # Enable Idle Mode
     xset +dpms
     xset dpms 120 120 120
-    xautolock -enable
+    
+    # Launch xidlehook in the background
+    # --not-when-audio and --not-when-fullscreen are the magic flags!
+    xidlehook \
+      --not-when-audio \
+      --not-when-fullscreen \
+      --timer 60 "slock" "" \
+      --timer 60 "xset dpms force off" "" \
+      --timer 180 "systemctl suspend" "" &
+      
     notify-send -u normal "Idle Mode" "Enabled: Lock (1m) -> Off (2m) -> Sleep (5m)."
 fi

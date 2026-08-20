be5a50e374f5ddf99f48352b635044551627675a
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-20 18:23:39 +0300

    Update idle-toggle.sh

--- config/scripts/idle-toggle.sh
+++ config/scripts/idle-toggle.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Check if xidlehook is running
 if pgrep -x "xidlehook" > /dev/null; then
-    # Disable Idle Mode
     killall xidlehook
     xset -dpms
     xset s off
+    touch /tmp/caffeine
     notify-send -u normal "Caffeine Mode" "Enabled: Screen will not turn off."
 else
-    # Enable Idle Mode
     xset -dpms
     xset s off
-    
-    # Launch xidlehook in the background
-    # --not-when-audio and --not-when-fullscreen are the magic flags!
+    rm -f /tmp/caffeine
     xidlehook \
       --not-when-audio \
       --not-when-fullscreen \
       --timer 60 "slock" "" \
       --timer 240 "systemctl suspend" "" &
-      
     notify-send -u normal "Idle Mode" "Enabled: Lock (1m) -> Sleep (5m)."
 fi

51588afecb60c5cc20d73c8a4f3060ea8c6d3d1c
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-26 22:07:48 +0300

    Update downloader.sh

--- config/scripts/downloader.sh
+++ config/scripts/downloader.sh
@@ -1,38 +1,7 @@
-#!/bin/bash
-
-CONFIG_FILE="$HOME/.config/rofi/rofi-downloader.conf"
-DEFAULT_DIR="$HOME/Downloads"
-
-if [ -f "$CONFIG_FILE" ]; then
-    SAVE_DIR=$(cat "$CONFIG_FILE")
-else
-    SAVE_DIR="$DEFAULT_DIR"
-fi
-
-if [ ! -d "$SAVE_DIR" ]; then
-    SAVE_DIR="$DEFAULT_DIR"
-fi
-
-OPTIONS="MP3 Audio (yt-dlp)\nMP4 Video (yt-dlp)\nWeb Page HTML (monolith)\nGit Clone Repository\nStandard File Download (wget)\nChange Download Path (Current: $SAVE_DIR)"
-CHOICE=$(echo -e "$OPTIONS" | rofi -normal-window -dmenu -p "Action" -i)
-
-if [ -z "$CHOICE" ]; then
-    exit 0
-fi
-
-if [[ "$CHOICE" == *"Change Download Path"* ]]; then
-    NEW_DIR=$(rofi -normal-window -dmenu -p "Enter Full Path" -i)
-    if [ -n "$NEW_DIR" ]; then
-        eval REAL_DIR="$NEW_DIR"
-        mkdir -p "$REAL_DIR"
-        echo "$REAL_DIR" > "$CONFIG_FILE"
-        notify-send "Downloader" "Path updated to: $REAL_DIR"
-    fi
-    exit 0
 fi
 
 CLIP=$(xclip -selection clipboard -o 2>/dev/null || wl-paste 2>/dev/null)
-if [[ "$CLIP" =~ ^https?:// ]]; then
+if [[ "$CLIP" =~ ^(https?|magnet): ]]; then
     PREFILL="$CLIP"
 else
     PREFILL=""
@@ -45,36 +14,42 @@ if [ -z "$URL" ]; then
 fi
 
 case "$CHOICE" in
-    "MP3 Audio (yt-dlp)")
+    "MP3 Audio")
         CMD="cd '$SAVE_DIR' && yt-dlp -x --audio-format mp3 --no-playlist '$URL'"
         ;;
-    "MP4 Video (yt-dlp)")
+    "MP3 Playlist")
+        CMD="cd '$SAVE_DIR' && yt-dlp -x --audio-format mp3 --yes-playlist '$URL'"
+        ;;
+    "MP4 Video")
         CMD="cd '$SAVE_DIR' && yt-dlp -S ext:mp4:m4a --no-playlist '$URL'"
         ;;
-    "Web Page HTML (monolith)")
+    "MP4 Playlist")
+        CMD="cd '$SAVE_DIR' && yt-dlp -S ext:mp4:m4a --yes-playlist '$URL'"
+        ;;
+    "Media / Images")
+        CMD="cd '$SAVE_DIR' && yt-dlp '$URL'"
+        ;;
+    "Torrent / Magnet")
+        CMD="cd '$SAVE_DIR' && aria2c --seed-time=0 '$URL'"
+        ;;
+    "Web Page HTML")
         FILENAME=$(echo "$URL" | sed -e 's/[^A-Za-z0-9]/_/g' | cut -c1-50)
         CMD="cd '$SAVE_DIR' && monolith '$URL' -o '${FILENAME}.html'"
         ;;
     "Git Clone Repository")
         CMD="cd '$SAVE_DIR' && git clone --recursive '$URL'"
         ;;
-    "Standard File Download (wget)")
+    "File Download")
         CMD="cd '$SAVE_DIR' && wget --content-disposition --show-progress '$URL'"
         ;;
 esac
 
-EXEC_CMD="$CMD; echo ''; echo 'Finished! Press Enter to exit...'; read"
+EXEC_CMD="$CMD; echo ''; echo 'Finished! Press Enter to exit.'; read"
 
-if command -v kitty &>/dev/null; then
-    kitty sh -c "$EXEC_CMD"
-elif command -v alacritty &>/dev/null; then
-    alacritty -e sh -c "$EXEC_CMD"
-elif command -v foot &>/dev/null; then
-    foot sh -c "$EXEC_CMD"
-elif command -v terminator &>/dev/null; then
-    terminator -e "sh -c \"$EXEC_CMD\""
-else
-    xterm -e sh -c "$EXEC_CMD"
-fi
+alacritty -e sh -c "$EXEC_CMD"
 
-notify-send "Downloader" "Task completed successfully."
+notify-send "Download Complete"
+
+
+^G Help           ^O Write Out      ^F Where Is       ^K Cut            ^T Execute        ^C Location       M-U Undo          M-A Set Mark      M-] To Bracket    M-B Previous      ◂ Back            ^◂ Prev Word      ^A Home
+^X Exit           ^R Read File      ^\ Replace        ^U Paste          ^J Justify        ^

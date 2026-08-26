771fd0f7721304a7bad2923b030301caf312ec0c
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-26 13:24:56 +0300

    Update config.h

--- slock/config.h
+++ slock/config.h
@@ -2,9 +2,9 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] =   "#000000",   /* after initialization */
-    [INPUT] =  "#000000",   /* during input */
-    [FAILED] = "#FFFFFF",   /* wrong password */
+    [INIT] =   "#1e1e2e",   /* after initialization (Mocha Base) */
+    [INPUT] =  "#89b4fa",   /* during input (Mocha Blue) */
+    [FAILED] = "#f38ba8",   /* wrong password (Mocha Red) */
 };
 /* treat a cleared input like a wrong password */
 static const int failonclear = 1;

02986f3909fc413b5545ffcf26f3122e8015abe8
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-08-28 11:16:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ sudo pacman -S --needed --noconfirm \
     thunar rofi imv btop playerctl alacritty thunar-archive-plugin xarchiver zip unzip polkit-gnome \
     xclip maim ttf-jetbrains-mono-nerd noto-fonts-emoji ttf-nerd-fonts-symbols \
     gtk3 pavucontrol nwg-look mpv brightnessctl xsettingsd nano vim \
-    xorg-xrandr power-profiles-daemon python-gobject arandr picom \
+    xorg-xrandr power-profiles-daemon python-gobject arandr picom libreoffice-fresh \
     lightdm lightdm-gtk-greeter dunst xorg-xinput qalculate-gtk aria2 \
     curl jq xdg-utils libnotify xorg-xset librewolf imagemagick audacious \
     clipmenu xsel xdotool ttf-dejavu ttf-font-awesome noto-fonts monolith \

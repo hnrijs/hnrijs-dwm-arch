021b08e659673a1ded965d2ee8526fe2f43ee94b
hnrijs <198484681+hnrijs@users.noreply.github.com>
2026-09-05 19:45:36 +0300

    Update system_update.sh

--- config/scripts/system_update.sh
+++ config/scripts/system_update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo " Starting Full System Update "
-echo "1. Updating Pacman packages..."
+echo "Starting Full System Update"
+
+echo "Updating Pacman packages..."
 sudo pacman -Syu --noconfirm
 
-echo "2. Updating AUR packages..."
-yay -Sua --noconfirm
-
-echo " Update Complete! "
+echo "Update Complete!"
